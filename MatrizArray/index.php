171dabecbf69cfeb8a8ce964b4336f572d420900
Kris0707 <francokris005@gmail.com>
2024-12-02 15:26:39 -0300

    corregido error 8

--- MatrizArray/index.php
+++ MatrizArray/index.php
@@ -49,8 +49,8 @@ do {
     } elseif ($opcion == 4) {
         // Mostrar temperaturas de todos los años de un mes
         echo "Ingrese el mes: ";
-        $mes = intval(trim(fgets(STDIN)));
-        $tempMensual = mostrarTemperaturasMensuales($matrizTemperatura, $mes);
+        $mes = trim(fgets(STDIN));
+        $tempMensual = mostrarTemperaturasMensuales($matrizTemperatura, $mes, $meses);
         mostrarMatriz($tempMensual, $años,$meses);
     } elseif ($opcion == 5) {
         // Hallar máximas y mínimas
@@ -67,7 +67,7 @@ do {
     } elseif ($opcion == 8) {
         // Arreglo asociativo
         $arregloAsociativo = crearArregloAsociativo($matrizTemperatura);
-        mostrarMatriz($arregloAsociativo, $años, $meses);
+        // mostrarMatriz($arregloAsociativo, $años, $meses);
     } 
     echo "¿Desea continuar?(Si/No)";
     $continuar = trim(fgets(STDIN));
@@ -117,7 +117,15 @@ function crearArregloAsociativo($matrizTemperatura) {
     $arreglo["Completa"] = $matrizTemperatura;
     $arreglo["Primavera"] = crearMatrizPrimavera($matrizTemperatura);
     $arreglo["Invierno"] = crearMatrizInvierno($matrizTemperatura);
-    return $arreglo;
+
+    echo "Matriz completa:\n";
+    print_r($arreglo["Completa"]);
+    
+    echo "\nMatriz de Primavera:\n";
+    print_r($arreglo["Primavera"]);
+
+    echo "\nMatriz de Invierno:\n";
+    print_r($arreglo["Invierno"]);
 }
 
 function tipoCarga($tipo, $años, $meses, &$matrizTemperatura) {
@@ -167,15 +175,15 @@ function mostrarTemperaturasAnuales($matriz, $año) {
     return ["año" => [$año], "matriz" => $matrizAnual];
 }
 
-function mostrarTemperaturasMensuales($matriz, $mes) {
-    $columna = $mes - 1;
-    $suma = 0;
-    for ($i = 0; $i < 10; $i++) {
-        echo "Año " . (2014 + $i) . ": " . $matriz[$i][$columna] . "<br>";
-        $suma += $matriz[$i][$columna];
-    }
-    $promedio = $suma / 10;
-    echo "Promedio del mes " . $mes . ": " . $promedio . "<br>";
+function mostrarTemperaturasMensuales($matriz, $mes, $mesesArray) {
+    $mesIndex = array_search($mes,  $mesesArray);
+    // $suma = 0;
+    // for ($i = 0; $i < 10; $i++) {
+    //     echo "Año " . (2014 + $i) . ": " . $matriz[$i][$columna] . "<br>";
+    //     $suma += $matriz[$i][$columna];
+    // }
+    // $promedio = $suma / 10;
+    // echo "Promedio del mes " . $mes . ": " . $promedio . "<br>";
 }
 
 function hallarExtremos($matriz) {
